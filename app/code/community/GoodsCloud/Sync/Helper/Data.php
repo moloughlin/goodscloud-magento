fd9fdd2856a74cf19ff3b3de81f47a16c8b609cc
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-14 18:21:56 +0200

    add helper to get store view settings

--- app/code/community/GoodsCloud/Sync/Helper/Data.php
+++ app/code/community/GoodsCloud/Sync/Helper/Data.php
@@ -2,5 +2,14 @@
 
 class GoodsCloud_Sync_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    public function getLanguageByStoreView(Mage_Core_Model_Store $view)
+    {
+        return strstr(Mage::getStoreConfig('general/locale/code', $view), '_', true);
+    }
 
+    public function getCurrencyByStoreView(Mage_Core_Model_Store $view)
+    {
+        $currentCurrency = Mage::app()->getStore($view)->getCurrentCurrencyCode();
+        return Mage::app()->getLocale()->currency($currentCurrency)->getShortName();
+    }
 }
