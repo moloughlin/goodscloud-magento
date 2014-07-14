b9f4ac3c03b79c0fd583d5a1db25d5ab82ff42ba
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-14 17:14:51 +0200

    model for first write from magento to goods cloud

---
+++ app/code/community/GoodsCloud/Sync/Model/FirstWrite.php
@@ -0,0 +1,35 @@
+<?php
+
+class GoodsCloud_Sync_Model_FirstWrite
+{
+    public function writeMagentoToGoodscloud()
+    {
+        // Add a Channel for every StoreView
+        $this->createChannelsFromStoreView();
+
+        // Add every AttributeSet as PropertySet to every Channel
+        $this->createPropertySetsFromAttributeSets();
+
+        // Add every Attribute as PropertySchema to every PropertySet
+        $this->createPropertySchemasFromAttributes();
+
+        // Copy the category tree to GoodsCloud
+        $this->createGCCategoriesFromCategories();
+    }
+
+    private function createChannelsFromStoreView()
+    {
+    }
+
+    private function createPropertySetsFromAttributeSets()
+    {
+    }
+
+    private function createPropertySchemasFromAttributes()
+    {
+    }
+
+    private function createGCCategoriesFromCategories()
+    {
+    }
+}
