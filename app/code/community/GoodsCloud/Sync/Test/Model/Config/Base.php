625d9648d02b07c1ae89c048abdef6fedb6ba6da
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-15 14:57:18 +0200

    add a few config assertions

---
+++ app/code/community/GoodsCloud/Sync/Test/Model/Config/Base.php
@@ -0,0 +1,21 @@
+<?php
+
+class GoodsCloud_Sync_Test_Model_Config_Base extends EcomDev_PHPUnit_Test_Case_Config
+{
+    public function testModel()
+    {
+        $this->assertModelAlias('goodscloud_sync/api', 'GoodsCloud_Sync_Model_Api');
+    }
+
+    public function testHelper()
+    {
+        $this->assertHelperAlias('goodscloud_sync', 'GoodsCloud_Sync_Helper_Data');
+    }
+
+    public function testConfig()
+    {
+        $this->assertModuleVersionGreaterThanOrEquals($this->expected('module')->getVersion());
+        $this->assertModuleCodePool($this->expected('module')->getCodePool());
+        $this->assertSchemeSetupScriptVersions('1.0.0', $this->expected('module')->getVersion());
+    }
+}
