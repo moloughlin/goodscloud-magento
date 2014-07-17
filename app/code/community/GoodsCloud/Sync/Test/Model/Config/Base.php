288cd7e1f4fb5d8eab5c661a869709815de7639b
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-17 16:48:08 +0200

    create attribute for categories to save goods cloud category id, add test for all install scripts, add new resource to use Mage_Eav Setup resource

--- app/code/community/GoodsCloud/Sync/Test/Model/Config/Base.php
+++ app/code/community/GoodsCloud/Sync/Test/Model/Config/Base.php
@@ -16,6 +16,11 @@ class GoodsCloud_Sync_Test_Model_Config_Base extends EcomDev_PHPUnit_Test_Case_C
     {
         $this->assertModuleVersionGreaterThanOrEquals($this->expected('module')->getVersion());
         $this->assertModuleCodePool($this->expected('module')->getCodePool());
-        $this->assertSchemeSetupScriptVersions('1.0.0', $this->expected('module')->getVersion());
+        $this->assertSchemeSetupScriptVersions(
+            '1.0.0', $this->expected('module')->getVersion(), null, 'goodscloud_sync_setup'
+        );
+        $this->assertSchemeSetupScriptVersions(
+            '1.0.0', $this->expected('module')->getVersion(), null, 'goodscloud_sync_eav_setup'
+        );
     }
 }
---
+++ app/code/community/GoodsCloud/Sync/sql/goodscloud_sync_eav_setup/upgrade-1.0.0-1.0.3.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this Mage_Catalog_Model_Resource_Setup */
+
+$applyToAllPhysical = array(
+    Mage_Catalog_Model_Product_Type::TYPE_SIMPLE,
+    Mage_Catalog_Model_Product_Type::TYPE_CONFIGURABLE,
+    Mage_Catalog_Model_Product_Type::TYPE_GROUPED,
+    Mage_Catalog_Model_Product_Type::TYPE_BUNDLE,
+);
+
+$this->addAttribute(
+    Mage_Catalog_Model_Category::ENTITY,
+    'gc_category_ids',
+    array(
+        'global'          => 1,
+        'required'        => 0,
+        'visible'         => 0,
+        'apply_to'        => implode(',', $applyToAllPhysical),
+        'is_configurable' => 0,
+    )
+);
