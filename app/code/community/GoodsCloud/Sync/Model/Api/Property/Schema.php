0a3797700a4301ff3817a2053bde6bdc7829096b
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-27 23:44:47 +0200

    Collection and model for property schema

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Property/Schema.php
@@ -0,0 +1,6 @@
+<?php
+
+class GoodsCloud_Sync_Model_Api_Property_Schema extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Property/Schema/Collection.php
@@ -0,0 +1,6 @@
+<?php
+
+class GoodsCloud_Sync_Model_Api_Property_Schema_Collection extends Varien_Data_Collection
+{
+
+}
