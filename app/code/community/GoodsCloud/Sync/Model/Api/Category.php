f24e2022287e2faac4249669c978c9e38414ccd1
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-28 00:12:37 +0200

    model and collection for category

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Category.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Category
+ */
+class GoodsCloud_Sync_Model_Api_Category extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Category/Collection.php
@@ -0,0 +1,6 @@
+<?php
+
+class GoodsCloud_Sync_Model_Api_Category_Collection extends Varien_Data_Collection
+{
+
+}
