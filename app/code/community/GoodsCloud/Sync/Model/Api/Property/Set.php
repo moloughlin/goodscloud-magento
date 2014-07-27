f6f4744e40c859e8a134d9faf73c33024ed86e75
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-28 00:14:41 +0200

    model and collection for property set

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Property/Set.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Property_Set
+ */
+class GoodsCloud_Sync_Model_Api_Property_Set extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Property/Set/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Property_Set_Collection
+ */
+class GoodsCloud_Sync_Model_Api_Property_Set_Collection extends Varien_Data_Collection
+{
+
+}
