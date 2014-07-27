84dda8eb41b98328d44782801c8e0ee2265b3de5
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-07-27 23:45:00 +0200

    ProgrammingError message

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Exception/ProgrammingError.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Exception_ProgrammingError
+ */
+class GoodsCloud_Sync_Model_Api_Exception_ProgrammingError extends GoodsCloud_Sync_Model_Api_Exception_Exception
+{
+
+}
