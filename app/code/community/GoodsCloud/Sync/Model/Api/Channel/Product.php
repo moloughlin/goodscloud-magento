1a4d461e5626da47b607ff44ce8ecd583852075a
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-09-21 17:09:35 +0200

    add classes for channel products

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Channel/Product.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Channel_Product
+ */
+class GoodsCloud_Sync_Model_Api_Channel_Product extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Channel/Product/Collection.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Channel_Product_Collection
+ *
+ * This collection holds a list of goodscloud channel products
+ */
+class GoodsCloud_Sync_Model_Api_Channel_Product_Collection extends Varien_Data_Collection
+{
+
+}
