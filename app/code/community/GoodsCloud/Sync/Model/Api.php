57ed8144ba86d9d4b058fa8b8382b46b367d4fc0
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-09-29 23:51:55 +0200

    try to get the entity if it is not there, catch exception and handle

--- app/code/community/GoodsCloud/Sync/Model/Api.php
+++ app/code/community/GoodsCloud/Sync/Model/Api.php
@@ -133,6 +133,14 @@ class GoodsCloud_Sync_Model_Api
         $query = '';
         if (!empty($filters)) {
             $query = '?q=' . urlencode(json_encode($filters));
+        try {
+            $response = $this->api->get($requestPath, $params);
+        } catch (Exception $e) {
+            try {
+                throw $this->parseErrorMessage($e);
+            } catch (GoodsCloud_Sync_Model_Api_Exception_NoResultFound $e) {
+                return null;
+            }
         }
 
         Mage::log("GET $model", Zend_Log::DEBUG, 'goodscloud.log');
@@ -595,13 +603,15 @@ class GoodsCloud_Sync_Model_Api
      */
     private function parseErrorMessage(Exception $exception)
     {
-        $message = $exception->getMessage();
+        $msg = $exception->getMessage();
 
-        // IntegrityError:
-        //        API request failed (status code 400): (IntegrityError) duplicate key value violates unique constraint "channel_label_company_id_key"
-        //DETAIL:  Key (label, company_id)=(Default Store Viewasd, 24) already exists.
-        //    'INSERT INTO channel (quality_score, label, external_identifier, is_sales, is_inventory, currency_code, language_code, company_id, email_config_id, return_reasons, cancellation_reasons, notification_emails, version) VALUES (%(quality_score)s, %(label)s, %(external_identifier)s, %(is_sales)s, %(is_inventory)s, %(currency_code)s, %(language_code)s, %(company_id)s, %(email_config_id)s, %(return_reasons)s, %(cancellation_reasons)s, %(notification_emails)s, %(version)s) RETURNING channel.id' {'email_config_id': None, 'is_inventory': False, 'external_identifier': u'1', 'company_id': 24, 'quality_score': 0, 'return_reasons': ['other'], 'version': 1, 'is_sales': True, 'language_code': u'en', 'notification_emails': [], 'label': u'Default Store Viewasd', 'currency_code': u'EUR', 'cancellation_reasons': ['consumer', 'test order', 'stock error']}
-        if (preg_match('#API .* \(status code (\d*)\): \((.*)\) (.*)\nDETAIL:  (.*)\n(.*)#', $message, $matches)) {
+        if (strpos($msg, 'status code 404') !== false) {
+            throw new GoodsCloud_Sync_Model_Api_Exception_NoResultFound();
+        } elseif (preg_match('#API .* \(status code (\d*)\): \((.*)\) (.*)\nDETAIL:  (.*)\n(.*)#', $msg, $matches)) {
+            // IntegrityError:
+            //        API request failed (status code 400): (IntegrityError) duplicate key value violates unique constraint "channel_label_company_id_key"
+            //DETAIL:  Key (label, company_id)=(Default Store Viewasd, 24) already exists.
+            //    'INSERT INTO channel (quality_score, label, external_identifier, is_sales, is_inventory, currency_code, language_code, company_id, email_config_id, return_reasons, cancellation_reasons, notification_emails, version) VALUES (%(quality_score)s, %(label)s, %(external_identifier)s, %(is_sales)s, %(is_inventory)s, %(currency_code)s, %(language_code)s, %(company_id)s, %(email_config_id)s, %(return_reasons)s, %(cancellation_reasons)s, %(notification_emails)s, %(version)s) RETURNING channel.id' {'email_config_id': None, 'is_inventory': False, 'external_identifier': u'1', 'company_id': 24, 'quality_score': 0, 'return_reasons': ['other'], 'version': 1, 'is_sales': True, 'language_code': u'en', 'notification_emails': [], 'label': u'Default Store Viewasd', 'currency_code': u'EUR', 'cancellation_reasons': ['consumer', 'test order', 'stock error']}
             if ($matches[2] == 'IntegrityError') {
                 $exception = new GoodsCloud_Sync_Model_Api_Exception_IntegrityError(
                     $matches[3] . "\n" . $matches[4], $matches[1], $exception
@@ -610,7 +620,7 @@ class GoodsCloud_Sync_Model_Api
                 $exception->setLongDetails($matches[5]);
                 return $exception;
             }
-        } elseif (preg_match('#API .* \(status code (\d*)\): \((.*?)\) (.*)#', $message, $matches)) {
+        } elseif (preg_match('#API .* \(status code (\d*)\): \((.*?)\) (.*)#', $msg, $matches)) {
             // API request failed (status code 400): (ProgrammingError) can't adapt type 'dict' 'INSERT INTO property_schema (label, external_identifier, description, channel_id, type, values, multivalue, "default", units, min, max, visible, searchable, filterable, comparable) VALUES (%(label)s, %(external_identifier)s, %(description)s, %(channel_id)s, %(type)s, %(values)s, %(multivalue)s, %(default)s, %(units)s, %(min)s, %(max)s, %(visible)s, %(searchable)s, %(filterable)s, %(comparable)s) RETURNING property_schema.id' {'comparable': u'1', 'description': u'', 'searchable': u'1', 'min': None, 'default': None, 'max': None, 'external_identifier': u'92', 'visible': u'0', 'label': u'color', 'channel_id': u'126', 'multivalue': False, 'units': None, 'values': [{u'value': u'', u'label': u''}], 'type': u'enum', 'filterable': True}
             if ($matches[2] == 'ProgrammingError') {
                 $exception = new GoodsCloud_Sync_Model_Api_Exception_ProgrammingError(
@@ -621,6 +631,6 @@ class GoodsCloud_Sync_Model_Api
             }
         }
 
-        Mage::throwException('Unknown Error: ' . $message);
+        Mage::throwException('Unknown Error: ' . $msg);
     }
 }
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Exception/NoResultFound.php
@@ -0,0 +1,6 @@
+<?php
+
+class GoodsCloud_Sync_Model_Api_Exception_NoResultFound extends GoodsCloud_Sync_Model_Api_Exception_Exception
+{
+
+}
