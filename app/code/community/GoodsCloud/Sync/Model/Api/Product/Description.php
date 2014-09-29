52737a37fbe91d0b203e3add576baa95f47d6551
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-09-29 18:04:15 +0200

    create classes for product descriptions

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Product/Description.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Product_Description
+ */
+class GoodsCloud_Sync_Model_Api_Product_Description extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Product/Description/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Product_Description_Collection
+ */
+class GoodsCloud_Sync_Model_Api_Product_Description_Collection extends Varien_Data_Collection
+{
+
+}
