5f508eae7ab6af66d73e52ff3a0f1bb0cfa72a50
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-09-30 17:50:46 +0200

    created classes for consumer

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Consumer.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Consumer
+ */
+class GoodsCloud_Sync_Model_Api_Consumer extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Consumer/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Consumer_Collection
+ */
+class GoodsCloud_Sync_Model_Api_Consumer_Collection extends Varien_Data_Collection
+{
+
+}
