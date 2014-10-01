8e03d90bc7b249a96638e80637e50ee2b5280cb7
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-10-01 18:54:33 +0200

    objects to represent orders

---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Order.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Order
+ */
+class GoodsCloud_Sync_Model_Api_Order extends Varien_Object
+{
+
+}
---
+++ app/code/community/GoodsCloud/Sync/Model/Api/Order/Collection.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class GoodsCloud_Sync_Model_Api_Order_Collection
+ */
+class GoodsCloud_Sync_Model_Api_Order_Collection extends Varien_Data_Collection
+{
+
+}
