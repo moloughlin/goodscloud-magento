34a561b5bffcd4f6333dbdb0dcd49af1f6b8dade
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-10-31 13:37:59 +0100

    formatting

--- app/code/community/GoodsCloud/Sync/Test/Model/FirstWrite.php
+++ app/code/community/GoodsCloud/Sync/Test/Model/FirstWrite.php
@@ -33,29 +33,40 @@ class GoodsCloud_Sync_Test_Model_FirstWrite extends EcomDev_PHPUnit_Test_Case
         $firstWrite = Mage::getModel('goodscloud_sync/firstWrite');
 
         // write channels
-        $modelMock = $this->mockModel('goodscloud_sync/firstWrite_channels', array('createChannelsFromStoreviews'));
+        $modelMock = $this->mockModel(
+            'goodscloud_sync/firstWrite_channels',
+            array('createChannelsFromStoreviews')
+        );
         $modelMock->expects($this->once())
             ->method('createChannelsFromStoreviews');
 
-        $this->replaceByMock('model', 'goodscloud_sync/firstWrite_channels', $modelMock);
+        $this->replaceByMock(
+            'model', 'goodscloud_sync/firstWrite_channels', $modelMock
+        );
 
         // write property sets
         $modelMock = $this->mockModel(
-            'goodscloud_sync/firstWrite_propertySets', array('createPropertySetsFromAttributeSets')
+            'goodscloud_sync/firstWrite_propertySets',
+            array('createPropertySetsFromAttributeSets')
         );
         $modelMock->expects($this->once())
             ->method('createPropertySetsFromAttributeSets');
 
-        $this->replaceByMock('model', 'goodscloud_sync/firstWrite_propertySets', $modelMock);
+        $this->replaceByMock(
+            'model', 'goodscloud_sync/firstWrite_propertySets', $modelMock
+        );
 
         // write property schemas
         $modelMock = $this->mockModel(
-            'goodscloud_sync/firstWrite_propertySchemas', array('createPropertySchemasFromAttributes')
+            'goodscloud_sync/firstWrite_propertySchemas',
+            array('createPropertySchemasFromAttributes')
         );
         $modelMock->expects($this->once())
             ->method('createPropertySchemasFromAttributes');
 
-        $this->replaceByMock('model', 'goodscloud_sync/firstWrite_propertySchemas', $modelMock);
+        $this->replaceByMock(
+            'model', 'goodscloud_sync/firstWrite_propertySchemas', $modelMock
+        );
 
         // create category
         $modelMock = $this->mockModel(
@@ -64,7 +75,9 @@ class GoodsCloud_Sync_Test_Model_FirstWrite extends EcomDev_PHPUnit_Test_Case
         $modelMock->expects($this->once())
             ->method('createCategories');
 
-        $this->replaceByMock('model', 'goodscloud_sync/firstWrite_categories', $modelMock);
+        $this->replaceByMock(
+            'model', 'goodscloud_sync/firstWrite_categories', $modelMock
+        );
 
 
         $firstWrite->writeMagentoToGoodscloud();
