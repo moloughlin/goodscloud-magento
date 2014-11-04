ebf93ab924048eb7d5418e067591dfc90e642cc8
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-11-04 11:15:54 +0100

    add type hinting for methods in company and channel product

--- app/code/community/GoodsCloud/Sync/Model/Api/Channel/Product.php
+++ app/code/community/GoodsCloud/Sync/Model/Api/Channel/Product.php
@@ -2,6 +2,40 @@
 
 /**
  * Class GoodsCloud_Sync_Model_Api_Channel_Product
+ *
+ * @method int getId()  not NULL
+ * @method array getLogisticOrderItems()  List of LogisticOrderItem entries. Write-only, value not returned in API responses.
+ * @method array getLogisticReturnItems()  List of LogisticReturnItem entries. Write-only, value not returned in API responses.
+ * @method array getOrderItems()  List of OrderItem entries. Write-only, value not returned in API responses.
+ * @method array getStorageCellInventories()  List of StorageCellInventory entries. Cascade delete, delete-orphan.
+ * @method bool getActive()  not NULL    True
+ * @method int getAtp()  not NULL
+ * @method int getNotifyQuantity()  not NULL    0
+ * @method int getPackagingUnit()
+ * @method int getPhysicalQuantity()  not NULL
+ * @method string getProperties()  not NULL
+ * @method int getReservedQuantity()  not NULL
+ * @method int getSafetyQuantity()  not NULL    0
+ * @method string getSku()
+ * @method int getSoldQuantity()  not NULL
+ * @method int getStockedQuantity()  not NULL
+ * @method string getUpdated()  not NULL
+ * @method int getVersion()  not NULL    1
+ * @method int getAuditUserId()
+ * @method int getChannelId()  not NULL
+ * @method array getChannel()  Single Channel entry.
+ * @method int getChosenDescriptionId()
+ * @method array getChosenDescription()  Single ProductDescription entry.
+ * @method int getCompanyProductId()   not NULL
+ * @method array getCompanyProduct()   Single CompanyProduct entry.
+ * @method int getDiscountPriceListId()
+ * @method array getDiscountPriceList()  Single PriceList entry.
+ * @method int getPriceListId()
+ * @method array getPriceList()  Single PriceList entry.
+ * @method int getPropertySetId()
+ * @method array getPropertySet()  Single PropertySet entry.
+ * @method string getCreated()
+ * @method array getChosenImages()  List of ProductImage entries.
  */
 class GoodsCloud_Sync_Model_Api_Channel_Product extends Varien_Object
 {
--- app/code/community/GoodsCloud/Sync/Model/Api/Company/Product.php
+++ app/code/community/GoodsCloud/Sync/Model/Api/Company/Product.php
@@ -2,8 +2,43 @@
 
 /**
  * Class GoodsCloud_Sync_Model_Api_Company_Product
+ *
+ * @method int getId()
+ * @method array getAvailableDescriptions()
+ * @method array getAvailableImages()
+ * @method array getChannelProducts()
+ * @method array getCompanyProductViews()
+ * @method array getInventoryAgreements()
+ * @method array getPrices()
+ * @method array getSalesAgreements()
+ * @method bool getActive()
+ * @method int getAtp()
+ * @method int getAtpExternal()
+ * @method int getAtpInternal()
+ * @method string getCountryOfOrigin()
+ * @method string getDimensions()
+ * @method string getGtin()
+ * @method string getLabel()
+ * @method string getManufacturerCode()
+ * @method string getManufacturerName()
+ * @method bool getPhysical()
+ * @method int getPhysicalQuantity()
+ * @method string getProperties()
+ * @method bool getStocked()
+ * @method int getStockedQuantity()
+ * @method string getUpdated()
+ * @method int getVersion()
+ * @method int getAbstractProductId()
+ * @method array getAbstractProduct()
+ * @method int getAuditUserId()
+ * @method int getCompanyId()
+ * @method array getCompany()
+ * @method string getCreated()
+ * @method string getEan()
+ * @method string getUpc()
  */
 class GoodsCloud_Sync_Model_Api_Company_Product extends Varien_Object
 {
 
 }
+
