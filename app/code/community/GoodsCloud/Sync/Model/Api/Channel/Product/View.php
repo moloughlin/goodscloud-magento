6d33f6f2b150e769d2afdb5dcd850de9843af2d0
Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
2014-11-10 17:59:57 +0100

    add comments and methods hints

--- app/code/community/GoodsCloud/Sync/Model/Api/Channel/Product/View.php
+++ app/code/community/GoodsCloud/Sync/Model/Api/Channel/Product/View.php
@@ -2,6 +2,27 @@
 
 /**
  * Class GoodsCloud_Sync_Model_Api_Channel_Product_View
+ *
+ * @method int getId()
+ * @method array getSeo()
+ * @method array getProperties()
+ * @method int getSku()
+ * @method string getUpdated()
+ * @method int getVersion()
+ * @method bool getVisible()
+ * @method int getAuditUserId()
+ * @method int getChannelId()
+ * @method array getChannel()
+ * @method int getChosenDescriptionId()
+ * @method array getChosenDescription()
+ * @method int getCompanyProductViewId()
+ * @method array getCompanyProductView()
+ * @method int getPropertySetId()
+ * @method array getPropertySet()
+ * @method string getCreated()
+ * @method array getCategories()
+ * @method array getChosenImages()
+
  */
 class GoodsCloud_Sync_Model_Api_Channel_Product_View extends Varien_Object
 {
--- app/code/community/GoodsCloud/Sync/Model/Api/Company/Product/View.php
+++ app/code/community/GoodsCloud/Sync/Model/Api/Company/Product/View.php
@@ -1,5 +1,22 @@
 <?php
 
+/**
+ * Class GoodsCloud_Sync_Model_Api_Company_Product_View
+ *
+ * @method int getId()
+ * @method array getAvailableDescriptions()
+ * @method array getAvailableImages()
+ * @method array getChannelProductViews()
+ * @method array getCompanyProducts()
+ * @method string getLabel()
+ * @method string getPreviewImageFragment()
+ * @method string getUpdated()
+ * @method int getVersion()
+ * @method int getAuditUserId()
+ * @method int getCompanyId()
+ * @method array getCompany()
+ * @method string getCreated()
+ */
 class  GoodsCloud_Sync_Model_Api_Company_Product_View extends Varien_Object
 {
 
