adede0b66b66946a365ac53e069b2acf304a93ec
Matthew O'Loughlin <mattdevmail@gmail.com>
2015-07-11 13:08:23 +0930

    Following Magento SUPEE-6285, Adminhtml controllers require override of _isAllowed. More here: http://magento.stackexchange.com/questions/73646/access-denied-errors-after-installing-supee-6285

--- app/code/community/GoodsCloud/Sync/controllers/Adminhtml/GetAwsController.php
+++ app/code/community/GoodsCloud/Sync/controllers/Adminhtml/GetAwsController.php
@@ -8,4 +8,9 @@ class GoodsCloud_Sync_Adminhtml_GetAwsController
         $api = Mage::getModel('goodscloud_sync/api_factory')->getApi();
         $this->getResponse()->setBody($api->get_session()->auth->access);
     }
+
+    protected function _isAllowed()
+    {
+        return Mage::getSingleton('admin/session')->isAllowed('system/config/goodscloud_sync');
+    }
 }
